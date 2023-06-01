ef1171e7cd1528929101c10228147a86b8f2b880
Oleksiy12 <betbonnay@gmail.com>
2023-06-01 17:20:47 +0300

    update5

--- button.js
+++ button.js
@@ -1,95 +1,28 @@
-let resultNumber = document.getElementById("resultNumber");
 
-// dataset всіх чисел
+const resultNumber = document.getElementById("resultNumber");
+const buttonValue = document.getElementsByClassName("button");
 
-const zeroBtn = document.querySelector(".zero");
-const zeroValue = zeroBtn.dataset.zero;
-zeroBtn.addEventListener("click", ()=> {
-    resultNumber.textContent = zeroValue;
-})
+let resultDisplay = "";
+let buttonHandler = function(){
+    let value = this.getAttribute("data-value");
+    if(value === "AC"){
+        resultDisplay = "";
+        resultNumber.textContent = resultDisplay;
+    } else if (value === "="){
+        if (resultDisplay.includes("+") || resultDisplay.includes("-") || resultDisplay.includes("*") || resultDisplay.includes("/")) {
+            let result = eval(resultDisplay);
+            resultNumber.textContent = result; 
+            resultDisplay = result.toString();
+            resultDisplay = "";
+        }
+    } else {
+        if(resultDisplay.length < 10){
+            resultDisplay+=value;
+            resultNumber.textContent = resultDisplay;
+    }
+    }
+} 
 
-const oneBtn = document.querySelector(".one");
-oneBtn.addEventListener("click", ()=>{
-  const oneValue = oneBtn.dataset.one;
-  resultNumber.textContent = oneValue;
-})
-
-const twoBtn = document.querySelector(".two");
-const twoValue = twoBtn.dataset.two;
-twoBtn.addEventListener("click", ()=>{
-    resultNumber.textContent = twoValue;
-})
-
-const threeBtn = document.querySelector(".three");
-const threeValue = threeBtn.dataset.three;
-threeBtn.addEventListener("click", ()=>{
-    resultNumber.textContent = threeValue;
-})
-
-const fourBtn = document.querySelector(".four");
-const fourValue = fourBtn.dataset.four;
-fourBtn.addEventListener("click", ()=>{
-    resultNumber.textContent = fourValue;
-})
-
-const fiveBtn = document.querySelector(".five");
-const fiveValue = fiveBtn.dataset.five;
-fiveBtn.addEventListener("click", ()=>{
-    resultNumber.textContent = fiveValue;
-})
-
-const sixBtn = document.querySelector(".six");
-const sixValue = sixBtn.dataset.six;
-sixBtn.addEventListener("click", ()=>{
-    resultNumber.textContent = sixValue;
-})
-
-const sevenBtn = document.querySelector(".seven");
-const sevenValue = sevenBtn.dataset.seven;
-sevenBtn.addEventListener("click", ()=>{
-    resultNumber.textContent = sevenValue;
-})
-
-const eightBtn = document.querySelector(".eight");
-const eightValue = eightBtn.dataset.eight;
-eightBtn.addEventListener("click", ()=>{
-    resultNumber.textContent = eightValue;
-})
-
-const nineBtn = document.querySelector(".nine");
-const nineValue = nineBtn.dataset.nine;
-nineBtn.addEventListener("click", ()=>{
-    resultNumber.textContent = nineValue;
-})
-
-// dataset всіх знаків
-
-const divideBtn = document.querySelector(".divide");
-const divideValue = divideBtn.dataset.divide;
-
-const multiplyBtn = document.querySelector(".multiply");
-const multiplyValue = multiplyBtn.dataset.multiply;
-
-const minuseBtn = document.querySelector(".minus");
-const minusValue = minuseBtn.dataset.minus;
-
-const plusBtn = document.querySelector(".plus");
-const plusValue = plusBtn.dataset.plus;
-
-const commaBtn = document.querySelector(".comma");
-const commaValue = commaBtn.dataset.comma;
-
-const resultBtn = document.querySelector(".result");
-const resultValue = resultBtn.dataset.result;
-
-// dataset result и т.д.
-
-const percentBtn = document.querySelector(".percent");
-const percentValue = percentBtn.dataset.percent;
-
-const cleanBtn = document.querySelector(".clean");
-cleanBtn.addEventListener("click", ()=>{
-    resultNumber.textContent = 0;
-})
-
-
+    for (let i = 0; i < buttonValue.length; i+=1){
+    buttonValue[i].addEventListener("click", buttonHandler);
+}
